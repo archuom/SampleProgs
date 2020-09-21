3c4612eab8c24798b06697afa8a2939b53deb9cd
user <user@Archanalaptop>
2020-09-21 23:44:40 +0530

    Test Program

---
+++ testing/src/test/java/mypack/Test1.java
@@ -0,0 +1,18 @@
+package mypack;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+	WebDriverManager.chromedriver().setup();
+	System.setProperty("webdriver.chrome.silentOutput", "true");
+	ChromeDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("http://www.amazon.in");
+	
+	}
+
+}
